532f599170e028b28aa3ece0695b8aea22d978eb
Stark <lakshayastark2104@gmail.com>
2025-10-30 11:12:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import GooeyNav from './components/GooeyNav'; 
+
+function App() {
+  const navItems = [
+    { label: 'Home', href: '#home' },
+    { label: 'About', href: '#about' },
+    { label: 'Services', href: '#services' },
+    { label: 'Contact', href: '#contact' },
+  ];
+
+  return (
+    <>
+    <div
+      className="absolute inset-0 -z-10 bg-cover bg-center"
+      style={{ backgroundImage: "url('/assets/background.png')" }}
+    />
+    <div className="flex justify-center w-full ">
+      <GooeyNav 
+        items={navItems} 
+        initialActiveIndex={0} 
+      />
+    </div>
+    </>
+  );
+}
+
+export default App;
